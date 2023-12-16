d01e7cdd017b838b7e2d5d82d2608605aff19a6a
GromovAlex <gromovsan@yandex.ru>
2023-12-16 04:22:38 +0300

    new file update

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(2 * 3027 + 5 + 2 * 3025 + 5)
+import pandas as pd
+df = pd.DataFrame(
+    {
+        'data': [1,2,3,4,5,6,], 
+        'columns': [1,2,3,4,5,6]
+    }
+)
+df.rename(index={0:'column'}, columns = {'columns':'колонка'}, inplace = True)
+print(df)
+
